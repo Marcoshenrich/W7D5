b194d623d9aa6f2f1f484c992ebb4c5ab75709ad
Alex Luong <alex0707101@gmail.com>
2022-11-18 10:05:13 -0800

    Finished auth controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:destroy]
+    
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user 
+            login(@user)
+            redirect_to users_url
+        else
+            flash[:errors] = ["Username or Password is invalid"]
+            render :new
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def destroy
+        logout
+        redirect_to new_session_url
+    end
 end
